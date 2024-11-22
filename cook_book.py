85cbcaff798eec2c7d656fb56ff04b659e039da5
IrinaHG <IrinaHG@yandex.ru>
2024-11-22 20:43:52 +0400

    Code modification

--- cook_book.py
+++ cook_book.py
@@ -1,38 +1,40 @@
 import os
-file_path = os.path.join(os.getcwd(), 'recipes.txt')
-cook_book = {}
 
-with open(file_path,'r',encoding='utf-8') as f:
-    for line in f:
-        dish_name = line.strip()
-        ingridients_count = int(f.readline().strip())
-        ingridients = []
-        for idx in range(ingridients_count):
-            ingridient = f.readline().strip().split(' | ')
-            ingridients.append({
-                'ingredient_name': ingridient[0],
-                'quantity': int(ingridient[1]),
-                'measure': ingridient[2]
-            })
-        cook_book[dish_name] = ingridients
-        f.readline()
+def get_cook_book():
+    file_path = os.path.join(os.getcwd(), 'recipes.txt')
+    cook_book = {}
+
+    with open(file_path,'r',encoding='utf-8') as f:
+        for line in f:
+            dish_name = line.strip()
+            ingridients_count = int(f.readline().strip())
+            ingridients = []
+            for idx in range(ingridients_count):
+                ingridient = f.readline().strip().split(' | ')
+                ingridients.append({
+                    'ingredient_name': ingridient[0],
+                    'quantity': int(ingridient[1]),
+                    'measure': ingridient[2]
+                })
+            cook_book[dish_name] = ingridients
+            f.readline()            
+    return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = get_cook_book()
     shop_list = {}
-    for dish in cook_book:
-        if dish in dishes:
+    for dish in dishes:
+        if dish in cook_book.keys():
             for  item in cook_book[dish]:
-                #print(item)
                 if item['ingredient_name'] in shop_list.keys():
                     shop_list[item['ingredient_name']]['quantity'] += item['quantity'] * person_count
                 else:
                     shop_list[item['ingredient_name']] = {
                        'measure': item['measure'],
                        'quantity': item['quantity'] * person_count
-                    }
-    return shop_list
+                    }                    
+    print('Список покупок:')
+    for item in shop_list:
+        print(f'{item} - {shop_list[item]["quantity"]} {shop_list[item]["measure"]}')    
     
-shop = get_shop_list_by_dishes(['Омлет','Фахитос'], 3)
-print('Список покупок:')
-for item in shop:
-    print(f'{item} - {shop[item]["quantity"]} {shop[item]["measure"]}')
+get_shop_list_by_dishes(['Омлет','Омлет'],  2)
--- sort_files.py
+++ sort_files.py
@@ -1,27 +1,30 @@
 import os
-file_path = os.path.join(os.getcwd(), 'sorted')
-os.chdir(file_path)
 
-filesdict = {}
-with open('1.txt','r',encoding='utf-8') as f:
-    filesdict['1.txt'] = len(f.readlines())
-    
-with open('2.txt','r',encoding='utf-8') as f:
-    filesdict['2.txt'] = len(f.readlines())
-    
-with open('3.txt','r',encoding='utf-8') as f:
-    filesdict['3.txt'] = len(f.readlines())
-    
-fileslist = sorted(filesdict.items(), key=lambda x: x[1])
-sortdict = dict(fileslist)
+def sort_files():
+    files_txt = [f for f in os.listdir() if f.endswith('.txt')]
+    filesdict = {}
+    for filename in files_txt:
+        if filename != 'result.txt':
+            with open(filename,'r',encoding='utf-8') as f:
+                 filesdict[filename] = len(f.readlines())
+      
+    fileslist = sorted(filesdict.items(), key=lambda x: x[1])
+    return dict(fileslist)
 
-with open('result.txt','w',encoding='utf-8') as rf:
-    for file in sortdict:
-        rf.write(file)
-        rf.write('\n')
-        rf.write(str(sortdict[file]))
-        rf.write('\n')
-        with open(file,'r',encoding='utf-8') as f:
-            for line in f:
-                rf.write(line)
-        rf.write('\n')
+def write_file():
+    file_path = os.path.join(os.getcwd(), 'sorted')
+    os.chdir(file_path)
+    sortdict = sort_files()
+    with open('result.txt','w',encoding='utf-8') as rf:
+        for file in sortdict:
+            rf.write(file)
+            rf.write('\n')
+            rf.write(str(sortdict[file]))
+            rf.write('\n')
+            with open(file,'r',encoding='utf-8') as f:
+                for line in f:
+                    rf.write(line)
+            rf.write('\n')
+    print('Check file RESULT.TXT')
+        
+write_file()
